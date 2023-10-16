cbc856d463cb432ded0a69468ce7f20b01d5631f
justCodeBatch1 <justcode.codehustle@gmail.com>
2023-10-16 23:14:16 +0600

    first

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById("test1").innerHTML = "Changed Text";
+console.log("This is a log in console..."); 
+//alert("This is an alert ..."); 
+document.write("This is an example of document. write");
